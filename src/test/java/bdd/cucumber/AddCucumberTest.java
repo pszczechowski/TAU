433b4faece2cad1eed4586e20a0f7a9cf8782463
pszczechowski <s16096@pjwstk.edu.pl>
2019-11-15 20:51:51 +0100

    implementation of the scenario in the test

--- src/test/java/bdd/cucumber/AddCucumberTest.java
+++ src/test/java/bdd/cucumber/AddCucumberTest.java
@@ -1,4 +1,5 @@
 package bdd.cucumber;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -7,10 +8,11 @@ import s16096.model.CustomerOrder;
 import s16096.repository.WorkToDoneRepo;
 import s16096.service.CrudService;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matcher.*;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.hamcrest.CoreMatchers.equalTo;
 
 
 public class AddCucumberTest {
@@ -31,9 +33,35 @@ public class AddCucumberTest {
     public void setOrderedPizza(ArrayList<String> orderedPizzas) {
         customerOrder.setOrderedPizzas(orderedPizzas);
     }
+
     @Then("^user can show (.*)$")
         public void orderPizzasIsCorrect(ArrayList<String> orderedPizzas){
         MatcherAssert.assertThat(customerOrder.getOrderedPizzas(), equalTo(orderedPizzas));
         }
+
+    @Given("^init order list$")
+    public void initOrderList() {
+        ArrayList<String> order1 = new ArrayList<String>();
+        ArrayList<String> order2 = new ArrayList<String>();
+        ArrayList<String> order3 = new ArrayList<String>();
+        order1.add("hawajska");
+        order2.add("peperoni");
+        order3.add("italiana");
+
+        repository.collectionAccess().add(new CustomerOrder(1,order1));
+        repository.collectionAccess().add(new CustomerOrder(2, order2));
+        repository.collectionAccess().add(new CustomerOrder(3, order3));
+    }
+
+    @When("^user wanna order with (.*) hawajska$")
+    public void getOrderByRegex(String regex) {
+        customerOrders = crudService.getCustomerOrderByRegex(regex);
+    }
+
+    @Then("^user get all tasks with (.*) hawajska$")
+    public void orderByRegexPassed(String regex) {
+        MatcherAssert.assertThat(customerOrders.size(),equalTo(0));
+    }
+
 
 }
--- src/test/resources/bdd/scenario_customerOrder.feature
+++ src/test/resources/bdd/scenario_customerOrder.feature
@@ -5,3 +5,10 @@ Given a new order
 When order get own pizzas <listOfOrder>
 Then user can show <listOfOrder>
 
+Scenario: Get order by regex
+Given init order list
+When user wanna order with <listOrder> hawajska
+Then user get all tasks with <listOrder> hawajska
+
+
+
